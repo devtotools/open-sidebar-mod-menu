2af610c05046675b4bdb5256fcae2441f156da1f
Thomas Bnt <thomasbnt@protonmail.com>
2023-03-09 19:23:36 +0100

    ✨ If the post is not published, don't show the mod panel

--- AutoOpenSidebarMenu.user.js
+++ AutoOpenSidebarMenu.user.js
@@ -15,12 +15,14 @@
 
 // See at https://github.com/forem/forem/blob/main/app/javascript/actionsPanel/initializeActionsPanelToggle.js#L19
 
-(function() {
-    'use strict';
-document
-      .getElementById('mod-actions-menu-btn-area')
-      .classList.remove('hidden');
-    document
-      .getElementsByClassName('mod-actions-menu')[0]
-      .classList.toggle('showing');
+(function () {
+  "use strict";
+  // If in the URL we have ?preview=, don't open the menu
+  if (window.location.href.indexOf("?preview=") > -1) return false;
+  document
+    .getElementById("mod-actions-menu-btn-area")
+    .classList.remove("hidden");
+  document
+    .getElementsByClassName("mod-actions-menu")[0]
+    .classList.toggle("showing");
 })();
