e619ca0b0f74b22504e6459bf4e5dafb86862c5a
Thomas Bnt <thomasbnt@protonmail.com>
2023-03-10 23:49:59 +0100

    ✨ Work with others Forems

--- AutoOpenSidebarMenu.user.js
+++ AutoOpenSidebarMenu.user.js
@@ -1,11 +1,28 @@
 // ==UserScript==
 // @name Automatically open the Mod panel on DEV.
 // @namespace https://dev.to
-// @version 0.5
+// @version 0.6
 // @description Automatically open the Mod panel on DEV.
 // @author Thomas Bnt
 // @icon https://icons.duckduckgo.com/ip3/dev.to.ico
 // @match https://dev.to/*
+// @match https://community.codenewbie.org/*
+// @match https://forem.dev/*
+// @match https://community.ops.io/*
+// @match https://community.interledger.org/*
+// @match https://www.wasm.builders/*
+// @match https://community.webcomponents.dev/*
+// @match https://forem.julialang.org/*
+// @match https://community.tidelift.com/*
+// @match https://www.coss.community/*
+// @match https://www.metapunk.to/*
+// @match https://www.joinforest.com/*
+// @match https://the.community.club/*
+// @match https://www.thismmalife.com/*
+// @match https://www.1vibe.com/*
+// @match https://www.flowstate.to/*
+// @match https://aws.newbie.tips/*
+// @match https://www.developmenthackers.com/*
 // @homepageURL https://github.com/devtotools/open-sidebar-mod-menu
 // @updateURL    https://raw.githubusercontent.com/devtotools/open-sidebar-mod-menu/main/AutoOpenSidebarMenu.user.js
 // @downloadURL  https://raw.githubusercontent.com/devtotools/open-sidebar-mod-menu/main/AutoOpenSidebarMenu.user.js
@@ -17,8 +34,10 @@
 
 (function () {
   "use strict";
+
   // If in the URL we have ?preview=, don't open the menu
   if (window.location.href.indexOf("?preview=") > -1) return false;
+
   document
     .getElementById("mod-actions-menu-btn-area")
     .classList.remove("hidden");
